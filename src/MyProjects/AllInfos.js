d85b6095b949993af814b6645d5269216ad938a1
Unknown-lad <jobsofsouvik@gmai.com>
2021-07-14 07:38:11 +0530

    updated project

--- src/MyProjects/AllInfos.js
+++ src/MyProjects/AllInfos.js
@@ -6,6 +6,8 @@ import FoodyCums from './img/foody-cums.jpeg';
 import FormValidation from './img/form-validation.jpeg';
 import MusicApp from './img/music-app.jpeg';
 import TicTacToe from './img/tic-tac-toe.jpeg';
+import ImgSearch from './img/imageSearch.jpeg';
+import QuizApp from './img/QuizApp.jpeg';
 
 const datas =[
     {
@@ -20,52 +22,66 @@ const datas =[
         title: "Simple Website Project",
         subtitle: "It is a simple HTML and CSS based website, It tells about auto-mobile college details",
         img: `${AutoSchool}`,
-        link: 'https://unknown-lad-2.github.io/Calculator/',
+        link: 'https://unknown-lad-2.github.io/Cars-Site/',
     },
     {
         id: 3,
+        title: "Images Search Project",
+        subtitle: "It is a simple React JS application, which fetch images form an API",
+        img: `${ImgSearch}`,
+        link: 'https://unknown-lad-2.github.io/ImageSearchEngine/',
+    },
+    {
+        id: 4,
+        title: "Quiz App Project",
+        subtitle: "Quiz app fully dynamic made up using Javascirpt, HTML and CSS, It performs all activites",
+        img: `${QuizApp}`,
+        link: 'https://unknown-lad-2.github.io/Quiz-App/',
+    },
+    {
+        id: 5,
         title: "Weather Reporter Project",
         subtitle: "This is an application based project with help of API, it tells the weather details",
         img: `${WeathrReptor}`,
         link: 'https://unknown-lad-2.github.io/Weather-Reporter/',
     },
     {
-        id: 4,
+        id: 6,
         title: "Simple Calculator Project",
         subtitle: "Like simple calculator this application can also perform basic functions of calculations",
         img: `${Calculator}`,
-        link: 'https://unknown-lad-2.github.io/Cars-Site/',
+        link: 'https://unknown-lad-2.github.io/Calculator/',
     },
     {
-        id: 5,
+        id: 7,
         title: "Todo List Project",
         subtitle: "A simple Todo List Application which performs basics functinos",
         img: `${TodoList}`,
         link: 'https://unknown-lad-2.github.io/Todo-list/',
     },
     {
-        id: 6,
+        id: 8,
         title: "Music App Project",
         subtitle: "This application can performs play, pause, next fucntion with songs like music player",
         img: `${MusicApp}`,
         link: 'https://unknown-lad-2.github.io/Music-Player/',
     },
     {
-        id: 7,
+        id: 9,
         title: "Tic-Tac-Toe Project",
         subtitle: "It can perfom all tasks and it fully dynamic with 2 player Mode",
         img: `${TicTacToe}`,
         link: 'https://unknown-lad-2.github.io/Tic-Tac-Toe/',
     },
     {
-        id: 8,
+        id: 10,
         title: "Form Validation Project",
         subtitle: "It can do all validation operation which a form need to check to complete it",
         img: `${FormValidation}`,
         link: 'https://unknown-lad-2.github.io/Form-Data-Storing/',
     },
     {
-        id: 9,
+        id: 11,
         title: "Todo List Project",
         subtitle: "Lorem Ipsum is simply dummy text of the printing and typesetting industry.",
         img: `${TodoList}`,
